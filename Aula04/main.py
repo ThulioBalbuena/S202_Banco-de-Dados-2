aaa9b11e5a09919bb0cf43ce754e829dfc33b390
Thulio Balbuena <thuliobalbuena@hotmail.com>
2024-03-17 15:45:58 -0300

    adding aula04

---
+++ Aula04/main.py
@@ -0,0 +1,46 @@
+from database import Database
+from helper.writeAJson import writeAJson
+
+db = Database(database="mercado", collection="produtos")
+
+class ProductAnalyzer:
+    result = db.collection.aggregate([
+        {
+        "$unwind": "$produtos"
+    },
+    {
+        "$group": {
+            "_id": "$data_compra",
+            "total_products_sold": {"$sum": "$produtos.quantidade"}
+        }
+    }
+])
+    writeAJson(result, "Total de vendas por dia")
+
+
+    result = db.collection.aggregate([
+        {"$unwind": "$produtos"},
+        {"$group": {"_id": "$produtos.descricao", "total": {"$sum": "$produtos.quantidade"}}},
+        {"$sort": {"total": -1}},
+        {"$limit": 1}
+    ])
+    writeAJson(result, "Produto mais vendido")
+
+
+    result = db.collection.aggregate([
+        {"$unwind": "$produtos"},
+        {"$group": {"_id": "$cliente_id", "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+        {"$sort": {"total": -1}},
+        {"$limit": 1}
+    ])
+    writeAJson(result, "Cliente que mais gastou")
+
+    result = db.collection.aggregate([
+        {"$unwind": "$produtos"},
+        {"$match": {"produtos.quantidade": {"$gt": 1}}}, 
+        {"$group": {"_id": "$produtos.descricao"}}
+    ])
+
+    writeAJson(result, "Produtos que venderam mais de 1 unidade")
+
+
