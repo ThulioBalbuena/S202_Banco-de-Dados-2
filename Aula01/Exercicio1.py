63538ebe2feb6ec1daab24a5e4f448791c1b87ce
Thulio Balbuena <thuliobalbuena@hotmail.com>
2024-04-15 18:52:19 -0300

    adicionar exemplos

--- Aula01/Exercicio1.py
+++ Aula01/Exercicio1.py
@@ -34,3 +34,19 @@ aula = Aula(professor, "Programação Orientada a Objetos")
 aula.adicionar_aluno(aluno1)
 aula.adicionar_aluno(aluno2)
 print(aula.listar_presenca())
+
+# exemplos mongodb:
+#db.paises.find({},{ "name.official": 1, "_id": 0 }).filter({ "region": "Americas", "area": { $lt: 100 } }) Mostrar apenas o nome oficial dos países que são da região "Americas" e possuem área menor que 100:
+#db.paises.find({},{ "name.common": 1, "_id": 0 }).filter({ "name.common": { $regex: "^B", $options: "i" } }).sort({ "area": 1 })Mostrar apenas o nome comum dos países que começam com "B" e ordenar os resultados por ordem crescente de área:
+#db.paises.find({},{ "name.common": 1, "latlng": 1, "_id": 0 }).filter({ "languages": "por" }) Mostrar apenas o nome comum dos países que começam com "B" e ordenar os resultados por ordem crescente de área:
+#db.paises.find({},{ "name.common": 1, "borders": 1, "_id": 0 }).filter({ "borders": { $size: 0 } }) Mostrar apenas o nome comum dos países que não possuem fronteiras:
+#db.paises.find({},{ "name.common": 1, "borders": 1, "_id": 0 }).filter({ "borders": { $size: 1 } }) Mostrar apenas o nome comum dos países que possuem apenas uma fronteira:
+
+#exemplos neo4j:
+#MATCH (p:Person) WHERE p.name = 'Tom Hanks' RETURN p
+#MATCH (m:Movie) WHERE m.title = 'The Matrix' RETURN m
+#MATCH (p:Person)-[:ACTED_IN]->(m:Movie) WHERE m.title = 'The Matrix' RETURN p
+
+
+
+
